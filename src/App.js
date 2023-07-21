48f3eb77d339f4d5245665034b970ac353a5edb0
Cristina <cristina.ionela.delisle@gmail.com>
2023-07-21 11:31:02 +0200

    code update without correct indentation and info displayed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const API_URL = "http://localhost:8080/api/v1/help";
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(API_URL).then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
+  const renderEndpoints = (endpoints) => {
+    return Object.keys(endpoints).map((endpointName) => (
+      <div key={endpointName}>
+        <h3>{endpointName}</h3>
+        <p>Help Path: {endpoints[endpointName].helpPath}</p>
+        <p>Brief: {endpoints[endpointName].brief}</p>
+      </div>
+    ));
+  };
+
+  const renderDescriptions = (descriptions) => {
+    return descriptions.map((desc, index) => (
+      <p key={index}>Description: {desc}</p>
+    ));
+  };
+
+  const renderCategory = (category, categoryData, indentLevel = 1) => {
+    const indentation = `${(indentLevel - 1) * 20}px`; // Adjust indentation as needed
+    return (
+      <div key={category} style={{ marginLeft: indentation }}>
+        <h2>{category}</h2>
+        {categoryData.description && renderDescriptions(categoryData.description)}
+        {categoryData.endpoints && renderEndpoints(categoryData.endpoints)}
+        {categoryData.categories &&
+          Object.entries(categoryData.categories).map(([subCategory, subCategoryData]) =>
+            renderCategory(subCategory, subCategoryData, indentLevel + 1)
+          )}
+      </div>
+    );
+  };
+
+  if (!data) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.description && <h1>Description: {data.description[0]}</h1>}
+      {data.endpoints && (
+        <div>
+          <h1>Endpoints:</h1>
+          {renderEndpoints(data.endpoints)}
+        </div>
+      )}
+      {data.categories && (
+        <div>
+          <h1>Categories:</h1>
+          {Object.entries(data.categories).map(([category, categoryData]) =>
+            renderCategory(category, categoryData)
+          )}
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
