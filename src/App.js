fb9643dad6b9217d9e44595538ba222dc1a00211
Cristina <cristina.ionela.delisle@gmail.com>
2023-07-26 18:34:59 +0200

    updates with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';C
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const API_URL = "http://localhost:8080/api/v1/help";
 
